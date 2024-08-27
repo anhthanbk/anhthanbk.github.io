9c9da510bd518111dd9d0decb3fc46fb3fe0013b
anhthanbk <anhthanbk@gmail.com>
2024-08-27 15:13:34 +0700

    Finish index

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,8 +1,9 @@
+//================= slide show
 // Khai báo biến
-const listImage = document.querySelector(".list-images");
-const imgs = listImage.getElementsByTagName("img");
-const btnLeft = document.querySelector(".arrow-left");
-const btnRight = document.querySelector(".arrow-right");
+const listImage = document.querySelector('.list-images');
+const imgs = listImage.getElementsByTagName('img');
+const btnLeft = document.querySelector('.arrow-left');
+const btnRight = document.querySelector('.arrow-right');
 const length = imgs.length;
 let current = 0;
 
@@ -12,54 +13,54 @@ const handleChangeSlide = () => {
     current = 0;
     let width = imgs[0].offsetWidth;
     listImage.style.transform = `translateX(0px)`;
-    document.querySelector(".active").classList.remove("active");
-    document.querySelector(".index-item-" + current).classList.add("active");
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.index-item-' + current).classList.add('active');
   } else {
     current++;
     let width = imgs[0].offsetWidth;
     listImage.style.transform = `translateX(${width * -1 * current}px)`;
-    document.querySelector(".active").classList.remove("active");
-    document.querySelector(".index-item-" + current).classList.add("active");
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.index-item-' + current).classList.add('active');
   }
 };
 let handleEvent = setInterval(handleChangeSlide, 4000);
 
 // Chạy các công thức
-btnRight.addEventListener("click", () => {
+btnRight.addEventListener('click', () => {
   clearInterval(handleEvent);
   handleChangeSlide();
   handleEvent = setInterval(handleChangeSlide, 4000);
 });
 
-btnLeft.addEventListener("click", () => {
+btnLeft.addEventListener('click', () => {
   clearInterval(handleEvent);
   if (current == 0) {
     current = length - 1;
     let width = imgs[0].offsetWidth;
     listImage.style.transform = `translateX(${width * -1 * current}px)`;
-    document.querySelector(".active").classList.remove("active");
-    document.querySelector(".index-item-" + current).classList.add("active");
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.index-item-' + current).classList.add('active');
   } else {
     current--;
     let width = imgs[0].offsetWidth;
     listImage.style.transform = `translateX(${width * -1 * current}px)`;
-    document.querySelector(".active").classList.remove("active");
-    document.querySelector(".index-item-" + current).classList.add("active");
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.index-item-' + current).classList.add('active');
   }
   handleEvent = setInterval(handleChangeSlide, 4000);
 });
 
-// tìm kiếm
+//===================== tìm kiếm
 function search() {
   // Lấy giá trị tìm kiếm từ input
-  let searchTerm = document.getElementById("search-input").value.toLowerCase();
+  let searchTerm = document.getElementById('search-input').value.toLowerCase();
 
   // Xóa các phần nổi bật trước đó
-  let paragraphs = document.getElementsByTagName("p");
+  let paragraphs = document.getElementsByTagName('p');
   for (let paragraph of paragraphs) {
     paragraph.innerHTML = paragraph.innerHTML.replace(
       /<span class="highlight">|<\/span>/g,
-      ""
+      ''
     );
   }
 
@@ -90,65 +91,72 @@ function search() {
   }
 }
 
-// menu
-document.getElementById("toggle").addEventListener("click", function () {
-  var navbarList = document.querySelector(".navbar-list");
-  if (navbarList.style.display === "none" || navbarList.style.display === "") {
-    navbarList.style.display = "block";
+//================= menu
+document.getElementById('toggle').addEventListener('click', function () {
+  var navbarList = document.querySelector('.navbar-list');
+  if (navbarList.style.display === 'none' || navbarList.style.display === '') {
+    navbarList.style.display = 'block';
   } else {
-    navbarList.style.display = "none";
+    navbarList.style.display = 'none';
   }
 });
 
-// detailed slide
-// slide 1
-const slides1 = document.querySelector(".sld-1");
-const slideCount1 = document.querySelectorAll(".slide").length;
-const slideWidth1 = document.querySelector(".slide").clientWidth;
-const totalWidth1 = slideWidth1 * slideCount1;
+//=========== detailed slide
+// Slide 1
+const initSlider = (sliderClass, btnLeftClass, btnRightClass) => {
+  const listImage1 = document.querySelector(sliderClass);
+  const imgs1 = listImage1.getElementsByTagName('img');
+  const btnLeft1 = document.querySelector(btnLeftClass);
+  const btnRight1 = document.querySelector(btnRightClass);
+  const length1 = imgs1.length;
+  let current1 = 0;
 
-let index1 = 0;
+  const handleChangeSlide1 = () => {
+    if (current1 == length1 - 1) {
+      current1 = 0;
+      let width1 = imgs1[0].offsetWidth;
+      listImage1.style.transform = `translateX(0px)`;
+    } else {
+      current1++;
+      let width1 = imgs1[0].offsetWidth;
+      listImage1.style.transform = `translateX(${width1 * -1 * current1}px)`;
+    }
+  };
 
-document.querySelector(".nxt-1").addEventListener("click", () => {
-  index1++;
-  if (index1 >= slideCount1 / 3) index1 = 0;
-  updateSlidePosition1();
-});
+  btnRight1.addEventListener('click', () => {
+    handleChangeSlide1();
+  });
 
-document.querySelector(".pr-1").addEventListener("click", () => {
-  index1--;
-  if (index1 < 0) index1 = Math.ceil(slideCount1 / 3) - 1;
-  updateSlidePosition1();
-});
-function updateSlidePosition1() {
-  const offset1 = -index1 * slideWidth1; // Di chuyển 3 ảnh một lần
-  slides1.style.transform = `translateX(${offset1 + 20}px)`;
-}
+  btnLeft1.addEventListener('click', () => {
+    if (current1 == 0) {
+      current1 = length1 - 1;
+      let width1 = imgs1[0].offsetWidth;
+      listImage1.style.transform = `translateX(${width1 * -1 * current1}px)`;
+    } else {
+      current1--;
+      let width1 = imgs1[0].offsetWidth;
+      listImage1.style.transform = `translateX(${width1 * -1 * current1}px)`;
+    }
+  });
+};
 
-// slide 2
-const slides2 = document.querySelector(".sld-2");
-const slideCount2 = document.querySelectorAll(".slide").length;
-const slideWidth2 = document.querySelector(".slide").clientWidth;
-const totalWidth2 = slideWidth2 * slideCount2;
+//Khởi tạo 2 slide khác nhau
+initSlider('.sld-1', '.pr-1', '.nxt-1');
+initSlider('.sld-2', '.pr-2', '.nxt-2');
+initSlider('.sld-3', '.pr-3', '.nxt-3');
+initSlider('.sld-4', '.pr-4', '.nxt-4');
+initSlider('.sld-5', '.pr-5', '.nxt-5');
+initSlider('.sld-6', '.pr-6', '.nxt-6');
 
-let index2 = 0;
-
-document.querySelector(".nxt-2").addEventListener("click", () => {
-  index2++;
-  if (index2 >= slideCount2 / 3) index2 = 0;
-  updateSlidePosition2();
-});
-
-document.querySelector(".pr-2").addEventListener("click", () => {
-  index2--;
-  if (index2 < 0) index2 = Math.ceil(slideCount2 / 3) - 1;
-  updateSlidePosition2();
-});
-function updateSlidePosition2() {
-  const offset2 = -index2 * slideWidth2; // Di chuyển 3 ảnh một lần
-  slides2.style.transform = `translateX(${offset2 + 20}px)`;
-}
-// Khi người dùng nhấp vào nút, di chuyển về đầu trang
+//========= Khi người dùng nhấp vào nút, di chuyển về đầu trang
 function topFunction() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+// =======cuộn đến class chỉ định trong trang
+const scrollToClass = (scrollTarget) => {
+  const element = document.querySelector(scrollTarget);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+};
